da2de82e0d225a3992cd63c4e1aa8710e4fa342c
robin_sacher <110891559+robinsacher@users.noreply.github.com>
2023-09-08 11:00:54 +0200

    Add files via upload

---
+++ Numberguesser/Multiplayer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Threading;
+
+namespace Numberguesser
+{
+    internal class Multiplayer
+    {
+        public void Play()
+        {
+            bool newGame = true;
+
+            while (newGame)
+            {
+                int counterPlayer1 = 0;
+                int counterPlayer2 = 0;
+
+                Random random = new Random();
+                int randomNumber1 = random.Next(1, 101);
+                int randomNumber2 = random.Next(1, 101);
+
+                bool again = true;
+                while (again)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Player 1, enter a number!");
+                    Console.WriteLine();
+                    try
+                    {
+                        int guessPlayer1 = Convert.ToInt32(Console.ReadLine());
+                        counterPlayer1++;
+
+                        // Vergleich der Zahlen
+                        if (guessPlayer1 == randomNumber1)
+                        {
+                            //ChatGPT
+                            int frequency = 1000;
+                            int duration = 1000;
+                            Console.Beep(frequency, duration);
+                            Thread.Sleep(duration);
+                            //-------------------------------
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Player 1 needed {counterPlayer1} tries");
+                            Console.WriteLine("Congratulations, Player 1 guessed the number!");
+                            Console.ResetColor();
+                            again = false;
+                        }
+                        else
+                        {
+                            if (guessPlayer1 < randomNumber1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Player 1: Your number was too small");
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Player 1: Your number was too big");
+                            }
+                            Console.ResetColor();
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        // Fehlermeldung anzeigen und zur erneuten Eingabe auffordern
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong input format! Please enter a valid number.");
+                        Console.ResetColor();
+                        Console.WriteLine("");
+                    }
+                }
+
+                again = true; // Wert zurücksetzen für Spieler 2 
+
+                while (again)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Player 2, enter a number!");
+                    Console.WriteLine();
+                    try
+                    {
+                        int guessPlayer2 = Convert.ToInt32(Console.ReadLine());
+                        counterPlayer2++;
+
+                        // Vergleich der Zahlen
+                        if (guessPlayer2 == randomNumber2)
+                        {
+                            //ChatGPT
+                            int frequency = 1000;
+                            int duration = 1000;
+                            Console.Beep(frequency, duration);
+                            Thread.Sleep(duration);
+                            //-------------------------------
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Player 2 needed {counterPlayer2} tries");
+                            Console.WriteLine("Congratulations, Player 2 guessed the number!");
+                            Console.ResetColor();
+
+                            if (counterPlayer1 < counterPlayer2)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("");
+                                Console.WriteLine("Player 1 won the game");
+                                Console.ResetColor();
+                            }
+                            else if (counterPlayer2 < counterPlayer1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("");
+                                Console.WriteLine("Player 2 won the game");
+                                Console.ResetColor();
+                            }
+                            again = false;
+                        }
+                        else
+                        {
+                            if (guessPlayer2 < randomNumber2)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Player 2: Your number was too small");
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Player 2: Your number was too big");
+                            }
+                            Console.ResetColor();
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        // Fehlermeldung anzeigen und zur erneuten Eingabe auffordern
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong input format! Please enter a valid number.");
+                        Console.ResetColor();
+                        Console.WriteLine("");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to play again (yes/no)?");
+                Console.WriteLine();
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                {
+                    newGame = true;
+                }
+                else if (answer == "no")
+                {
+                    newGame = false;
+                }
+                else
+                {
+                    throw new Exception();
+                } 
+            }
+        }
+    }
+}
---
+++ Numberguesser/Singleplayer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace Numberguesser
+{
+    internal class Singleplayer
+    {
+        public void Play()
+        {
+            bool newGame = true;
+
+            while (newGame)
+            {
+                int counter = 0;
+                System.Random random = new System.Random();
+                int randomNumber = random.Next(1, 101);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter a number!");
+                Console.WriteLine();
+
+                bool again = true;
+                while (again)
+                {
+                    try
+                    {
+                        int guess = Convert.ToInt32(Console.ReadLine());
+                        counter++;
+
+                        // Vergleich der Zahlen
+                        if (guess == randomNumber)
+                        {
+                            //ChatGPT
+                            int frequency = 1000;
+                            int duration = 1000;
+                            Console.Beep(frequency, duration) ;
+                            Thread.Sleep(duration);
+                            //-------------------------------
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("You needed " + counter +  " tries");
+                            Console.WriteLine("Congratulations, you guessed the number!");
+                            Console.WriteLine("You won");
+                            Console.ResetColor();
+                            again = false;
+                        }
+                        else
+                        {
+                            if (guess < randomNumber)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Your number was too small");
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Your number was too big");
+                            }
+
+                            Console.ResetColor();
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        // Fehlermeldung anzeigen und zur erneuten Eingabe auffordern
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong input format! Please enter a valid number.");
+                        Console.ResetColor();
+                        Console.WriteLine("");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to play again (yes/no)?");
+                Console.WriteLine();
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                {
+                    newGame = true; 
+                }
+                else if (answer == "no") 
+                {
+                    newGame = false;
+                }
+                else 
+                {
+                    newGame = false;
+                    throw new Exception();
+                }    
+            }
+        }
+    }
+}
